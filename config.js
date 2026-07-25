cf4e0b09cb78e4755e3af540c7bad8944a58d6bb
Ben Clifford <ben.w.clifford@gmail.com>
2026-07-25 15:43:35 -0700

    Update config to complete migration to the new Cognito table

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const backendConfigs = {
   dev: {
     apiUrl: "https://ij9i8u1wvg.execute-api.us-west-2.amazonaws.com",
     region: "us-west-2",
-    userPoolClientId: "7tlba3kd4kv5p4e1h5363s7a29",
-    userPoolId: "us-west-2_lTDVLzK6E",
+    userPoolClientId: "7pjs6emoi0akjaa9f1o9vmc3lr",
+    userPoolId: "us-west-2_WBmJJT1YQ",
   },
   prod: {
     apiUrl: "https://55g413zjq2.execute-api.us-west-2.amazonaws.com",
     region: "us-west-2",
-    userPoolClientId: "3jucb7dgsgteq2v98ae3uoacmq",
-    userPoolId: "us-west-2_6BjuamntD",
+    userPoolClientId: "ofvkvrb4qmqi47nburkqsmud7",
+    userPoolId: "us-west-2_JJcEs8uhA",
   },
 };
 
